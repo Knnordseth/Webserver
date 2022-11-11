9e220f3632b392c33160f21b86a21dbb89199c29
root <root@DESKTOP-MOQJCG0.localdomain>
2022-11-11 14:14:35 +0100

    lagt til knapp for bg osv

--- javascript.js
+++ javascript.js
@@ -2,3 +2,10 @@ function button1Click(farge) {
     document.getElementById("tekst").setAttribute("style","color:" + farge); 
    
 }
+function generiskknapp(type,farge) {
+    if (type= "tekstfarge") {
+        document.getElementById("tekst").setAttribute("style","color:" + farge); 
+    } else if (type= "bgfarge") {
+        document.getElementById("body").setAttribute("style","background-color:" + farge); 
+    }
+}
