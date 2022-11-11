ef919b5fa9663240df340b6e740527c756c983c7
root <root@DESKTOP-MOQJCG0.localdomain>
2022-11-11 14:54:14 +0100

    bytta if statement fra = til ===, fiksa syntax

--- javascript.js
+++ javascript.js
@@ -3,9 +3,9 @@ function button1Click(farge) {
    
 }
 function generiskknapp(type,farge) {
-    if (type= "tekstfarge") {
+    if (type=== "tekstfarge") {
         document.getElementById("tekst").setAttribute("style","color:" + farge); 
-    } else if (type= "bgfarge") {
+    } else if (type=== "bgfarge") {
         document.getElementById("body").setAttribute("style","background-color:" + farge); 
     }
 }
